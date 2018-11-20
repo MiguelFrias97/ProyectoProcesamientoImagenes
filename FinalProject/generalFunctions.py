b2fff69f14710b388ccb61798d983d9353504027
MiguelFrias <migue_tj_frias@hotmail.com>
2018-11-20 14:12:59 -0800

    Adding functionality to color detection. Color detection by HSV and HSL

--- FinalProject/generalFunctions.py
+++ FinalProject/generalFunctions.py
@@ -4,38 +4,57 @@ import matplotlib.pyplot as plt
 
 flags = [i for i in dir(cv2) if i.startswith('COLOR_')]
 
-def detectColor(img,color):
+def detectColor(img,color,colorModel):
     """
     Given an image detects the given color. Return the mask.
     :param img: numpy Array
     :param color: tuple
+    :param colorModel: int 0:HSV, 1:HSL
     :return: numpy Array
     """
-    img = cv2.cvtcolor(img,cv2.COLOR_BGR2RGB)
-    img = cv2.cvtcolor(img,cv2.COLOR_RGB2HSL)
-    mask = cv2.inRange(img,color[0],color[1])
-    img = cv2.bitwise_and(img,50,200)
-    img = np.asarray(img,dtype='float32')/255.0
-
-    return img
-
-def detectColors(image,colors):
-    """
-    Given an image detects the given colors. Return the mask.
-    :param image: numpy Array
-    :param colors: list
-    :return: numpy Array
-    """
-    result = np.zeros_like(image,'float32')
-    for color in colors:
-        img = image.copy()
+    if colorModel == 0:
+        img = cv2.cvtcolor(img,cv2.COLOR_BGR2RGB)
+        img = cv2.cvtcolor(img,cv2.COLOR_RGB2HSV)
+        mask = cv2.inRange(img,color[0],color[1])
+        img = cv2.bitwise_and(img,50,200)
+        img = np.asarray(img,dtype='float32')/255.0
+    else:
         img = cv2.cvtcolor(img,cv2.COLOR_BGR2RGB)
         img = cv2.cvtcolor(img,cv2.COLOR_RGB2HSL)
         mask = cv2.inRange(img,color[0],color[1])
         img = cv2.bitwise_and(img,50,200)
         img = np.asarray(img,dtype='float32')/255.0
-        result += img
-        
+
+    return img
+
+def detectColors(image,colors,colorModel):
+    """
+    Given an image detects the given colors. Return the mask.
+    :param image: numpy Array
+    :param colors: list
+    :param colorModel: int 0:HSV, 1:HSL
+    :return: numpy Array
+    """
+    result = np.zeros_like(image,'float32')
+    if colorModel == 0:
+        for color in colors:
+            img = image.copy()
+            img = cv2.cvtcolor(img,cv2.COLOR_BGR2RGB)
+            img = cv2.cvtcolor(img,cv2.COLOR_RGB2HSV)
+            mask = cv2.inRange(img,color[0],color[1])
+            img = cv2.bitwise_and(img,50,200)
+            img = np.asarray(img,dtype='float32')/255.0
+            result += img
+        else: 
+            for color in colors:
+            img = image.copy()
+            img = cv2.cvtcolor(img,cv2.COLOR_BGR2RGB)
+            img = cv2.cvtcolor(img,cv2.COLOR_RGB2HSV)
+            mask = cv2.inRange(img,color[0],color[1])
+            img = cv2.bitwise_and(img,50,200)
+            img = np.asarray(img,dtype='float32')/255.0
+            result += img
+            
     return result
 
 def resize(img,scale):
