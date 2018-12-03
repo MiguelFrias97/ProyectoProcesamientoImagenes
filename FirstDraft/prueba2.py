1ca17a72fcb4d20ae236a1f043654bb027802148
Mario Carvajal <31300192+MarioCarvajal@users.noreply.github.com>
2018-12-03 14:59:03 -0800

    Add files via upload

--- FirstDraft/prueba2.py
+++ FirstDraft/prueba2.py
@@ -10,39 +10,21 @@ from scipy import signal
 import csv
 import pandas as pd
 
+
 def main():
     flags = [i for i in dir(cv2) if i.startswith('COLOR_')]
-    green = [120,255,255]
-    red = [0,255,255]
-    blue = [240,255,255]
-    yellow = [60,255,255]
 
-    lightyellow = np.asarray([25,50,50])
-    darkyellow = np.asarray([45,255,255])
-
-    lightred = np.asarray([0,50,50])
-    darkred = np.asarray([20,255,255])
-
-    lightgreen = np.asarray([50,50,50])
-    darkgreen = np.asarray([70,255,255])
-
-    lightblue= np.asarray([110,50,50])
-    darkblue = np.asarray([130,255,255])
-
-    colors = [[lightblue,darkblue],[lightyellow,darkyellow],[lightgreen,darkgreen],[lightred,darkred]]
-    tools = [['blue hammer.png','blue chainsaw.png'],['yellow chainsaw.png'],['green shovel.png'],['red shovel.png','red hammer.png']]
-
-    #img = prepareImage('imagen.png',lightWhite,darkWhite)
-    #template = prepareImage('imagen1.png',lightWhite,darkWhite)
+    colors = determineRanges()
+    tools = [['blue chainsaw.png','blue hammer.png'],['yellow chainsaw.png','yellow spring clamp.png'],['green shovel.png','green spring clamp.png'],['red shovel.png','red hammer.png']]
     img_name = captureImage()
+    #img_name = 'blue chainsaw pic.png'
     img, color = prepareImage(img_name,colors)
-
-    tool, x, y = detectTool(img,tools,colors,color)
-    #print(tool,x,y)
+    row = img.shape[0]
+    col = img.shape[1]
+    tool = detectTool(img,tools,colors,color)
     
     tool_type = tool.split(".")[0]
-    print(tool_type)
-
+    
     file_string = "test_file.csv"
 
     try:
@@ -59,17 +41,11 @@ def main():
     except:
         new_tool = {'Tool':tool_type, 'Qty':1}
         writeCSV(file_string, new_tool)
-
-    #bw = 10
-    #bh = 10
-
+    print(tool_type)
     img = cv2.cvtColor(img,cv2.COLOR_GRAY2RGB)
-    #cv2.rectangle(img,(x-bw,y-bh),(x+bw,y+bh),(0,0,255),3)
-    #cv2.rectangle(img,(x-bw,y-bh),(x+bw,x+bh),(0,0,255),3)
+    img = cv2.resize(img,(col*2,row*2))
     plt.imshow(img,cmap='gray')
     plt.show()
-    #img = cv2.resize(img,(col*3,row*3))
-    #cv2.imshow('Detection', img)
     
 def detectTool(img,tools,colors, color):
     tool = ''
@@ -82,33 +58,27 @@ def detectTool(img,tools,colors, color):
         
         template, irrel = prepareImage(tools[color][i],colors)
         imgR = signal.correlate2d(img,template,boundary='symm',mode='full')
-        
-        y,x=np.unravel_index(np.argmax(imgR),imgR.shape)
-        point.append([x,y])
-        row = imgR.shape[0]
-        col = imgR.shape[1]
+        row = img.shape[0]
+        col = img.shape[1]
         for p1 in range(row):
             for p2 in range(col):
                 if imgR[p1,p2] != 0:
                     value_Count[i]+=1
     tool_value = np.argmin(value_Count)
     tool = tools[color][tool_value]
-    x, y = point[tool_value][0],point[tool_value][1]
-    return tool, x, y
+    return tool
 
 def prepareImage(image,colors):
     a = cv2.imread(image)
-    row = a.shape[0]//3
-    col = a.shape[1]//3
+    row = a.shape[0]//2
+    col = a.shape[1]//2
     img = cv2.resize(a, (col,row))
     img = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-    ##img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    color = detectColor(img, colors)
-    mask = cv2.inRange(img,colors[color][0],colors[color][1])
-    #cv2.imshow("CASKINGO",mask)
+    mask, color = detectColor(img, colors)
+    #mask = cv2.inRange(img,colors[color][0],colors[color][1])
     img = cv2.bitwise_and(img, img, mask=mask)
+    
     img = cv2.Canny(img,100,200)
-
     img = np.asarray(img,dtype='float32')/255.0
     return img, color
 
@@ -135,24 +105,37 @@ def writeCSV(file, tool):
         writer.writerow(tool)
     
 def detectColor(img,colors):
+    value_Count = []
+    trys = []
     for i in range(len(colors)):
+        value_Count.append(0)
         attempt = cv2.inRange(img,colors[i][0],colors[i][1])
+        trys.append(attempt)
         row = attempt.shape[0]
         col = attempt.shape[1]
-        color = True
         for x in range(row):
-            if color:
                 for y in range(col):
                     if attempt[x,y] != 0:
-                        color = False
-            else:
-                value = i
-                break
-       
-        if color == False:
-            break
-    return value
+                        value_Count[i]+=1
+    color_value = np.argmax(value_Count)
+    attempt = trys[color_value]
+    return attempt, color_value
 
+def determineRanges():
+    lightyellow = np.asarray([25,50,50])
+    darkyellow = np.asarray([40,255,255])
+
+    lightred = np.asarray([0,50,50])
+    darkred = np.asarray([20,255,255])
+
+    lightgreen = np.asarray([50,50,50])
+    darkgreen = np.asarray([70,255,255])
+
+    lightblue= np.asarray([110,50,50])
+    darkblue = np.asarray([130,255,255])
+
+    colors = [[lightblue,darkblue],[lightyellow,darkyellow],[lightgreen,darkgreen],[lightred,darkred]]
+    return colors
 def captureImage():
     cam = cv2.VideoCapture(0)
     cv2.namedWindow("Image Capture")
